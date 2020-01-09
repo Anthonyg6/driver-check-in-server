87354314b3481067534a43c03fb53780d0e4cdc8
Anthonyg6 <anthonygallegos6@gmail.com>
2020-01-08 19:52:15 -0700

    seperate route logic into own file and clean up app.js file

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-const CheckIn = require("./models/check-in");
+// const CheckIn = require("./models/check-in");
+
+const checkInRoutes = require("./routes/checkIn");
 
 const app = express();
 
@@ -33,60 +35,5 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post("/check-in", (req, res, next) => {
-  let checkIn = new CheckIn({
-    driverName: req.body.driverName,
-    carrier: req.body.carrier,
-    deliveryType: req.body.deliveryType,
-    checkInTime: req.body.checkInTime
-  });
-  checkIn
-    .save()
-    .then(() => {
-      res.status(201).json({
-        message: "New Driver Check In post has been created!"
-      });
-    })
-    .catch(error => {
-      res.status(400).json({
-        error
-      });
-    });
-});
-
-app.delete("/check-in/:id", (req, res, next) => {
-  CheckIn.findOneAndDelete({ _id: req.params.id })
-    .then(() => {
-      res.status(200).json({ message: "Driver Check-In delete" });
-    })
-    .catch(error => {
-      res.status(400).json(error);
-    });
-});
-
-app.get("/check-in/:id", (req, res, next) => {
-  CheckIn.findOne({
-    _id: req.params.id
-  })
-    .then(CheckIn => {
-      res.status(200).json(CheckIn);
-    })
-    .catch(error => {
-      res.status(404).json({
-        error
-      });
-    });
-});
-
-app.use("/check-in", (req, res, next) => {
-  CheckIn.find()
-    .then(checkIns => {
-      res.status(200).json(checkIns);
-    })
-    .catch(error => {
-      res.status(400).json({
-        error
-      });
-    });
-});
+app.use("/check-in", checkInRoutes);
 module.exports = app;
---
+++ routes/checkIn.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const CheckIn = require("../models/check-in");
+
+const router = express.Router();
+
+router.post("/", (req, res, next) => {
+  let checkIn = new CheckIn({
+    driverName: req.body.driverName,
+    carrier: req.body.carrier,
+    deliveryType: req.body.deliveryType,
+    checkInTime: req.body.checkInTime
+  });
+  checkIn
+    .save()
+    .then(() => {
+      res.status(201).json({
+        message: "New Driver Check In post has been created!"
+      });
+    })
+    .catch(error => {
+      res.status(400).json({
+        error
+      });
+    });
+});
+
+router.delete("/:id", (req, res, next) => {
+  CheckIn.findOneAndDelete({ _id: req.params.id })
+    .then(() => {
+      res.status(200).json({ message: "Driver Check-In delete" });
+    })
+    .catch(error => {
+      res.status(400).json(error);
+    });
+});
+
+router.get("/:id", (req, res, next) => {
+  CheckIn.findOne({
+    _id: req.params.id
+  })
+    .then(CheckIn => {
+      res.status(200).json(CheckIn);
+    })
+    .catch(error => {
+      res.status(404).json({
+        error
+      });
+    });
+});
+
+router.get("/", (req, res, next) => {
+  CheckIn.find()
+    .then(checkIns => {
+      res.status(200).json(checkIns);
+    })
+    .catch(error => {
+      res.status(400).json({
+        error
+      });
+    });
+});
+
+module.exports = router;
