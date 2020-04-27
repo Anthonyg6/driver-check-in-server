4c87a3d7a0dff7e8f82429487f53d500466d985d
Anthonyg6 <anthonygallegos6@gmail.com>
2020-04-26 20:36:57 -0600

    made another change

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const http = require("http");
 const app = require("./app");
+const cors = require("cors");
 
+app.use(cors());
 app.set("port", process.env.port || 4000);
 const server = http.createServer(app);
 
