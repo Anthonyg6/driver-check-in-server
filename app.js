983701d0b9bc69d46334f02459b0d514ba1b5090
Anthonyg6 <anthonygallegos6@gmail.com>
2020-05-02 17:33:48 -0600

    updated app.js route

--- app.js
+++ app.js
@@ -11,8 +11,6 @@ const bodyParser = require("body-parser");
 const checkInRoutes = require("./routes/checkIn");
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use("/check-in", checkInRoutes);
 
 mongoose
   .connect(
@@ -38,5 +36,7 @@ app.use((req, res, next) => {
   );
   next();
 });
+app.use(bodyParser.json());
+app.use("/check-in", checkInRoutes);
 
 module.exports = app;
