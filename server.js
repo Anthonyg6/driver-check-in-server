62d9f9af59c4e0c97edb27f9d4b7b77efb82a13f
Anthonyg6 <anthonygallegos6@gmail.com>
2020-05-09 21:20:01 -0600

    hopefuly put the right port env var

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const app = require("./app");
 
-app.set("port", process.env.port || 4000);
+app.set("port", process.env.PORT || 4000);
 const server = http.createServer(app);
 
 app.listen(app.get("port"), () => {
