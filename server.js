62927093ac8e38f684798ca6fe571a1187354333
Anthonyg6 <anthonygallegos6@gmail.com>
2020-05-09 21:17:43 -0600

    running server through app instead of http createServer

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = require("./app");
 app.set("port", process.env.port || 4000);
 const server = http.createServer(app);
 
-server.listen(app.get("port"), () => {
+app.listen(app.get("port"), () => {
   console.log("Server up");
 });
